dcfe035fb9dc78bc2d8ac87f5e7c9e2fd12d06d9
andreafrancioni <andreafrancioni.work@gmail.com>
2023-11-06 18:12:38 +0100

    Creato primo esercizio PALINDROMA

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 const parola = prompt("Inserisci una parola.");
 
 
-function isPalindroma(parola, palindroma) {
+function isPalindroma(parola) {
     let palindroma = "";
 
     for ( let i=parola.length-1; i >= 0; i-- ){
 
        separa = parola[i];
-       console.log(separa);
        palindroma += separa;
     }
     return palindroma;
 }
 
-console.log(palindroma);
+verificaParola = isPalindroma(parola);
+
+if (parola == verificaParola){
+    console.log("LA PAROLA INSERITA É PALINDROMA")
+}else{
+    console.log("LA PAROLA INSERITA NON É PALINDROMA!")
+}
